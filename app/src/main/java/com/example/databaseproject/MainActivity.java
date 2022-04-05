9acc779186cfeb0da9a546d2e1eb33d1ea9ad3c5
lisaromanova <soika-peresmeshnica2015@yandex.ru>
2022-04-05 23:08:08 +0300

    First_commit

--- app/src/main/java/com/example/databaseproject/MainActivity.java
+++ app/src/main/java/com/example/databaseproject/MainActivity.java
@@ -6,17 +6,22 @@ import android.content.ContentValues;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.LinearLayout;
+import android.widget.TableLayout;
+import android.widget.TableRow;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     Button btnAdd, btnRead, btnClear;
     EditText etName, etEmail;
 
     DBHelper dbHelper;
+    SQLiteDatabase database;
+    ContentValues contentValues;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,10 +37,60 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         btnClear = (Button) findViewById(R.id.btnClear);
         btnClear.setOnClickListener(this);
 
-        etName = (EditText) findViewById(R.id.etName);
-        etEmail = (EditText) findViewById(R.id.etEmail);
+        etName = findViewById(R.id.etName);
+        etEmail = findViewById(R.id.etEmail);
 
         dbHelper = new DBHelper(this);
+        database = dbHelper.getWritableDatabase();
+        UpdateTable();
+    }
+
+    public void UpdateTable() {
+        Cursor cursor = database.query(DBHelper.TABLE_CONTACTS, null, null, null, null, null, null);
+
+        if (cursor.moveToFirst()) {
+            int idIndex = cursor.getColumnIndex(DBHelper.KEY_ID);
+            int nameIndex = cursor.getColumnIndex(DBHelper.KEY_NAME);
+            int emailIndex = cursor.getColumnIndex(DBHelper.KEY_MAIL);
+            TableLayout dbOutput = findViewById(R.id.dbOutput);
+            dbOutput.removeAllViews();
+            do {
+                TableRow dbOutputRow = new TableRow(this);
+                dbOutputRow.setLayoutParams(new LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT, LinearLayout.LayoutParams.WRAP_CONTENT));
+
+                TableRow.LayoutParams params = new TableRow.LayoutParams(TableRow.LayoutParams.WRAP_CONTENT, TableRow.LayoutParams.WRAP_CONTENT);
+
+                TextView outputID = new TextView(this);
+                params.weight = 1.0f;
+                outputID.setLayoutParams(params);
+                outputID.setText(cursor.getString(idIndex));
+                dbOutputRow.addView(outputID);
+
+                TextView outputName = new TextView(this);
+                params.weight = 3.0f;
+                outputName.setLayoutParams(params);
+                outputName.setText(cursor.getString(nameIndex));
+                dbOutputRow.addView(outputName);
+
+                TextView outputMail = new TextView(this);
+                params.weight = 3.0f;
+                outputMail.setLayoutParams(params);
+                outputMail.setText(cursor.getString(emailIndex));
+                dbOutputRow.addView(outputMail);
+
+                Button deleteBtn = new Button(this);
+                deleteBtn.setOnClickListener(this);
+                params.weight=1.0f;
+                deleteBtn.setLayoutParams(params);
+                deleteBtn.setText("Удалить запись");
+                deleteBtn.setId(cursor.getInt(idIndex));
+                dbOutputRow.addView(deleteBtn);
+
+                dbOutput.addView(dbOutputRow);
+
+            } while (cursor.moveToNext());
+        }
+        cursor.close();
     }
 
     @Override
@@ -44,14 +99,10 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         String name = etName.getText().toString();
         String email = etEmail.getText().toString();
 
-        SQLiteDatabase database = dbHelper.getWritableDatabase();
-
-        ContentValues contentValues = new ContentValues();
-
-
         switch (v.getId()) {
 
             case R.id.btnAdd:
+                contentValues = new ContentValues();
                 contentValues.put(DBHelper.KEY_NAME, name);
                 contentValues.put(DBHelper.KEY_MAIL, email);
 
@@ -59,27 +110,12 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                 break;
 
             case R.id.btnRead:
-                Cursor cursor = database.query(DBHelper.TABLE_CONTACTS, null, null, null, null, null, null);
-
-                if (cursor.moveToFirst()) {
-                    int idIndex = cursor.getColumnIndex(DBHelper.KEY_ID);
-                    int nameIndex = cursor.getColumnIndex(DBHelper.KEY_NAME);
-                    int emailIndex = cursor.getColumnIndex(DBHelper.KEY_MAIL);
-                    do {
-                        Log.d("mLog", "ID = " + cursor.getInt(idIndex) +
-                                ", name = " + cursor.getString(nameIndex) +
-                                ", email = " + cursor.getString(emailIndex));
-                    } while (cursor.moveToNext());
-                } else
-                    Log.d("mLog","0 rows");
-
-                cursor.close();
+                UpdateTable();
                 break;
 
             case R.id.btnClear:
                 database.delete(DBHelper.TABLE_CONTACTS, null, null);
                 break;
         }
-        dbHelper.close();
     }
 }
