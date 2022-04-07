919337d0048174ed1b56a7bbbc67c5e86b298a59
lisaromanova <soika-peresmeshnica2015@yandex.ru>
2022-04-07 21:37:28 +0300

    Third_commit

--- app/src/main/java/com/example/databaseproject/DBHelper.java
+++ app/src/main/java/com/example/databaseproject/DBHelper.java
@@ -9,12 +9,17 @@ public class DBHelper  extends SQLiteOpenHelper{
     public static final int DATABASE_VERSION = 1;
     public static final String DATABASE_NAME = "StudentsDB";
     public static final String TABLE_CONTACTS = "Students";
+    public static final String TABLE_USERS = "User";
 
     public static final String KEY_ID = "_id";
     public static final String KEY_SURNAME = "surname";
     public static final String KEY_NAME = "name";
     public static final String KEY_DISC = "disciplina";
     public static final String KEY_MARK = "mark";
+
+    public static final String KEY_ID1 = "_id";
+    public static final String KEY_LOGIN = "login";
+    public static final String KEY_PASSWORD = "password";
 
     public DBHelper(Context context) {
         super(context, DATABASE_NAME, null, DATABASE_VERSION);
@@ -24,13 +29,15 @@ public class DBHelper  extends SQLiteOpenHelper{
     public void onCreate(SQLiteDatabase db) {
         db.execSQL("create table " + TABLE_CONTACTS + "(" + KEY_ID
                 + " integer primary key," + KEY_SURNAME + " text," + KEY_NAME + " text,"+ KEY_DISC + " text," + KEY_MARK + " text"+")");
+        db.execSQL("create table " + TABLE_USERS + "(" + KEY_ID1
+                + " integer primary key," + KEY_LOGIN + " text," + KEY_PASSWORD + " text"+")");
 
     }
 
     @Override
     public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
         db.execSQL("drop table if exists " + TABLE_CONTACTS);
-
+        db.execSQL("drop table if exists " + TABLE_USERS);
         onCreate(db);
 
     }
--- app/src/main/java/com/example/databaseproject/MainActivity.java
+++ app/src/main/java/com/example/databaseproject/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.databaseproject;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.ContentValues;
+import android.content.Intent;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
@@ -14,161 +15,80 @@ import android.widget.LinearLayout;
 import android.widget.TableLayout;
 import android.widget.TableRow;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
-    Button btnAdd, btnClear;
-    EditText etName, etSurname, etDisc, etMark;
+    Button btnEnter, btnSign;
+    EditText username, password;
 
     DBHelper dbHelper;
     SQLiteDatabase database;
-    ContentValues contentValues;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        btnAdd = (Button) findViewById(R.id.btnAdd);
-        btnAdd.setOnClickListener(this);
+        btnEnter = (Button) findViewById(R.id.btnEnter);
+        btnEnter.setOnClickListener(this);
 
 
-        btnClear = (Button) findViewById(R.id.btnClear);
-        btnClear.setOnClickListener(this);
+        btnSign = (Button) findViewById(R.id.btnSign);
+        btnSign.setOnClickListener(this);
 
-        etName = findViewById(R.id.Name);
-        etSurname = findViewById(R.id.Surname);
-        etDisc = findViewById(R.id.Disc);
-        etMark = findViewById(R.id.Mark);
+        username = findViewById(R.id.Login);
+        password = findViewById(R.id.Password);
 
         dbHelper = new DBHelper(this);
         database = dbHelper.getWritableDatabase();
-        UpdateTable();
     }
 
-    public void UpdateTable() {
-        Cursor cursor = database.query(DBHelper.TABLE_CONTACTS, null, null, null, null, null, null);
-
-        if (cursor.moveToFirst()) {
-            int idIndex = cursor.getColumnIndex(DBHelper.KEY_ID);
-            int nameIndex = cursor.getColumnIndex(DBHelper.KEY_NAME);
-            int surnameIndex = cursor.getColumnIndex(DBHelper.KEY_SURNAME);
-            int discIndex = cursor.getColumnIndex(DBHelper.KEY_DISC);
-            int markIndex = cursor.getColumnIndex(DBHelper.KEY_MARK);
-            TableLayout dbOutput = findViewById(R.id.dbOutput);
-            dbOutput.removeAllViews();
-            do {
-                TableRow dbOutputRow = new TableRow(this);
-                dbOutputRow.setLayoutParams(new LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT, LinearLayout.LayoutParams.WRAP_CONTENT));
-
-                TableRow.LayoutParams params = new TableRow.LayoutParams(TableRow.LayoutParams.WRAP_CONTENT, TableRow.LayoutParams.WRAP_CONTENT);
-
-                TextView outputID = new TextView(this);
-                params.weight = 1.0f;
-                outputID.setLayoutParams(params);
-                outputID.setText(cursor.getString(idIndex));
-                dbOutputRow.addView(outputID);
-
-                TextView outputSurname = new TextView(this);
-                params.weight = 3.0f;
-                outputSurname.setLayoutParams(params);
-                outputSurname.setText(cursor.getString(surnameIndex));
-                dbOutputRow.addView(outputSurname);
-
-                TextView outputName = new TextView(this);
-                params.weight = 3.0f;
-                outputName.setLayoutParams(params);
-                outputName.setText(cursor.getString(nameIndex));
-                dbOutputRow.addView(outputName);
-
-                TextView outputDisc = new TextView(this);
-                params.weight = 3.0f;
-                outputDisc.setLayoutParams(params);
-                outputDisc.setText(cursor.getString(discIndex));
-                dbOutputRow.addView(outputDisc);
-
-                TextView outputMark = new TextView(this);
-                params.weight = 3.0f;
-                outputMark.setLayoutParams(params);
-                outputMark.setText(cursor.getString(markIndex));
-                dbOutputRow.addView(outputMark);
-
-                Button deleteBtn = new Button(this);
-                deleteBtn.setOnClickListener(this);
-                params.weight=1.0f;
-                deleteBtn.setLayoutParams(params);
-                deleteBtn.setText("Удалить запись");
-                deleteBtn.setId(cursor.getInt(idIndex));
-                dbOutputRow.addView(deleteBtn);
-
-                dbOutput.addView(dbOutputRow);
-
-            } while (cursor.moveToNext());
-        }
-        cursor.close();
-    }
 
     @Override
     public void onClick(View v) {
         switch (v.getId()) {
 
-            case R.id.btnAdd:
-                String name = etName.getText().toString();
-                String surname = etSurname.getText().toString();
-                String disc = etDisc.getText().toString();
-                String mark = etMark.getText().toString();
-                contentValues = new ContentValues();
-                contentValues.put(DBHelper.KEY_SURNAME, surname);
-                contentValues.put(DBHelper.KEY_NAME, name);
-                contentValues.put(DBHelper.KEY_DISC, disc);
-                contentValues.put(DBHelper.KEY_MARK, mark);
-                database.insert(DBHelper.TABLE_CONTACTS, null, contentValues);
-                UpdateTable();
-                etName.setText("");
-                etSurname.setText("");
-                etDisc.setText("");
-                etMark.setText("");
-                break;
+            case R.id.btnEnter:
+                Cursor loginCursor = database.query(DBHelper.TABLE_USERS, null, null, null, null, null, null);
+                boolean logged = false;
+                if (loginCursor.moveToFirst()) {
 
-            case R.id.btnClear:
-                database.delete(DBHelper.TABLE_CONTACTS, null, null);
-                TableLayout dbOutput = findViewById(R.id.dbOutput);
-                dbOutput.removeAllViews();
-                UpdateTable();
-                break;
-
-            default:
-                View outputDBRow = (View) v.getParent();
-                ViewGroup outputDB = (ViewGroup) outputDBRow.getParent();
-                outputDB.removeView(outputDBRow);
-                outputDB.invalidate();
-
-                database.delete(DBHelper.TABLE_CONTACTS, DBHelper.KEY_ID+" = ?", new String[]{String.valueOf((v.getId()))});
-                contentValues = new ContentValues();
-                Cursor cursorUpdater = database.query(DBHelper.TABLE_CONTACTS, null, null, null, null, null, null);
-                if(cursorUpdater.moveToFirst()) {
-                    int idIndex = cursorUpdater.getColumnIndex(DBHelper.KEY_ID);
-                    int nameIndex = cursorUpdater.getColumnIndex(DBHelper.KEY_NAME);
-                    int surnameIndex = cursorUpdater.getColumnIndex(DBHelper.KEY_SURNAME);
-                    int discIndex = cursorUpdater.getColumnIndex(DBHelper.KEY_DISC);
-                    int markIndex = cursorUpdater.getColumnIndex(DBHelper.KEY_MARK);
-                    int realID = 1;
+                    int usernameIndex = loginCursor.getColumnIndex(DBHelper.KEY_LOGIN);
+                    int passwordIndex = loginCursor.getColumnIndex(DBHelper.KEY_PASSWORD);
                     do {
-                        if (cursorUpdater.getInt(idIndex) > realID) {
-                            contentValues.put(DBHelper.KEY_ID, realID);
-                            contentValues.put(DBHelper.KEY_SURNAME, cursorUpdater.getString(surnameIndex));
-                            contentValues.put(DBHelper.KEY_NAME, cursorUpdater.getString(nameIndex));
-                            contentValues.put(DBHelper.KEY_DISC, cursorUpdater.getString(discIndex));
-                            contentValues.put(DBHelper.KEY_MARK, cursorUpdater.getString(markIndex));
-                            database.replace(DBHelper.TABLE_CONTACTS, null, contentValues);
+                        if (username.getText().toString().equals(loginCursor.getString(usernameIndex)) && password.getText().toString().equals(loginCursor.getString(passwordIndex))) {
+                            startActivity(new Intent(this, MainUser.class));
+                            logged = true;
+                            break;
                         }
-                        realID++;
-                    } while (cursorUpdater.moveToNext());
-                    if (cursorUpdater.moveToLast()) {
-                        database.delete(DBHelper.TABLE_CONTACTS, DBHelper.KEY_ID + " = ?", new String[]{cursorUpdater.getString(idIndex)});
-                    }
-                    UpdateTable();
+                    } while (loginCursor.moveToNext());
                 }
+                loginCursor.close();
+                if (!logged)
+                    Toast.makeText(this, "Введенная комбинация логина и пароля не была найдена", Toast.LENGTH_LONG).show();
+                break;
+            case R.id.btnSign:
+                Cursor signCursor = database.query(DBHelper.TABLE_USERS, null, null, null, null, null, null);
+                boolean finded = false;
+                if (signCursor.moveToFirst()) {
+                    int usernameIndex = signCursor.getColumnIndex(DBHelper.KEY_LOGIN);
+                    do {
+                        if (username.getText().toString().equals(signCursor.getString(usernameIndex))) {
+                            Toast.makeText(this, "Введеный вами логин уже зарегистрирован", Toast.LENGTH_LONG).show();
+                            finded = true;
+                            break;
+                        }
+                    } while (signCursor.moveToNext());
+                }
+                if (!finded) {
+                    ContentValues contentValues = new ContentValues();
+                    contentValues.put(DBHelper.KEY_LOGIN, username.getText().toString());
+                    contentValues.put(DBHelper.KEY_PASSWORD, password.getText().toString());
+                    database.insert(DBHelper.TABLE_USERS, null, contentValues);
+                    Toast.makeText(this, "Вы успешно зарегистрировались!", Toast.LENGTH_LONG).show();
+                }
+                signCursor.close();
                 break;
         }
     }
